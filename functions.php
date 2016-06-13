d52ff8568ff7da4052f0da77f0b094f51d80da13
Alexander Belyaev <sashabe@yandex.ru>
2016-06-14 01:19:12 +0300

    more hints and enhancements

--- functions.php
+++ functions.php
@@ -1,214 +1,224 @@
 <?php
-	/**
-	 * Starkers functions and definitions
-	 *
-	 * For more information on hooks, actions, and filters, see http://codex.wordpress.org/Plugin_API.
-	 *
- 	 * @package 	WordPress
- 	 * @subpackage 	Starkers
- 	 * @since 		Starkers 4.0
-	 */
+  /**
+   * Starkers functions and definitions
+   *
+   * For more information on hooks, actions, and filters, see http://codex.wordpress.org/Plugin_API.
+   *
+   * @package   WordPress
+   * @subpackage  Starkers
+   * @since     Starkers 4.0
+   */
 
-	/* ========================================================================================================================
-	
-	Required external files
-	
-	======================================================================================================================== */
+  /* ========================================================================================================================
+  
+  Required external files
+  
+  ======================================================================================================================== */
 
-	require_once( 'external/starkers-utilities.php' );
-
-
-
-	/* ========================================================================================================================
-	
-	Register scripts and CSS
-	
-	======================================================================================================================== */
-
-	function starkers_script_enqueuer() {
-		wp_register_script( 'site', get_template_directory_uri().'/js/scripts.min.js', array( 'jquery' ) );
-		wp_enqueue_script( 'site' );
-
-		wp_register_style( 'screen', get_stylesheet_directory_uri().'/style.css', '', '', 'screen' );
-		wp_enqueue_style( 'screen' );
-	}	
+  require_once( 'external/starkers-utilities.php' );
 
 
 
-	/* ========================================================================================================================
-	
-	Theme specific settings - theme support, menus, thumbnails
+  /* ========================================================================================================================
+  
+  Register scripts and CSS
+  
+  ======================================================================================================================== */
 
-	======================================================================================================================== */
+  function starkers_script_enqueuer() {
+    wp_register_script( 'site', get_template_directory_uri().'/js/scripts.min.js', array( 'jquery' ) );
+    wp_enqueue_script( 'site' );
 
-	//Add theme support for various features
-	add_theme_support('post-thumbnails');
-	add_theme_support('menus');
-	add_theme_support('widgets');
-	add_theme_support( 'html5', array( 'search-form' ) );
-
-
-	// Create menu locations
-	register_nav_menus(
-		array(
-			'mainmenu' => 'Menu in the header'
-			// ,'footermenu' => 'Menu in the footer'
-		)
-	);
-
-	// Main thumbnail size
-	// set_post_thumbnail_size( 80, 80, true );
-	//Custom post thumbnail sizes
-	// add_image_size( 'miniposter', 96, 130, true ); //(cropped)
-
-	//Register widget areas
-	function widgets_init_now() {
-		register_sidebar( array(
-			'name' => 'Правая колонка'
-			,'id' => 'aside-right'
-		) );
-	}
-	add_action( 'widgets_init', 'widgets_init_now' );
+    wp_register_style( 'screen', get_stylesheet_directory_uri().'/style.css', '', '', 'screen' );
+    wp_enqueue_style( 'screen' );
+  } 
 
 
 
-	/* ========================================================================================================================
-	
-	Actions and filters
-	
-	======================================================================================================================== */
+  /* ========================================================================================================================
+  
+  Theme specific settings - theme support, menus, thumbnails
 
-	add_action( 'wp_enqueue_scripts', 'starkers_script_enqueuer' );
+  ======================================================================================================================== */
 
-
-	add_filter( 'body_class', array( 'Starkers_Utilities', 'add_slug_to_body_class' ) );
-
-
-	//makes all classes in custom menu dissappear, except noted
-	function css_attributes_filter($var) {
-		return is_array($var) ? array_intersect($var, array('current-menu-item','current_page_item','current-page-ancestor','current-menu-ancestor','current-menu-parent')) : '';
-	}
-	add_filter('nav_menu_css_class', 'css_attributes_filter', 100, 1); 
-	add_filter('nav_menu_item_id', 'css_attributes_filter', 100, 1);
+  //Add theme support for various features
+  add_theme_support('post-thumbnails');
+  add_theme_support('menus');
+  add_theme_support('widgets');
+  add_theme_support( 'html5', array( 'search-form' ) );
 
 
+  // Create menu locations
+  register_nav_menus(
+    array(
+      'mainmenu' => 'Menu in the header'
+      // ,'footermenu' => 'Menu in the footer'
+    )
+  );
 
-	/* ========================================================================================================================
-	
-	Admin changes
-	
-	======================================================================================================================== */
+  // Main thumbnail size
+  // set_post_thumbnail_size( 80, 80, true );
+  //Custom post thumbnail sizes
+  // add_image_size( 'miniposter', 96, 130, true ); //(cropped)
 
-	//Remove comments from admin
-	function remove_menus(){
-		remove_menu_page( 'edit-comments.php' );//Comments
-	}
-	add_action( 'admin_menu', 'remove_menus' );
-
-
-
-	/* ========================================================================================================================
-	
-	Reusable functions
-	
-	======================================================================================================================== */
-
-	function custom_is_archive() {return (is_archive() || is_search());} // || is_page_template('template-custompage.php')
-
-
-	//Custom content function with words manual limit
-	function content($limit, $postid, $showmorelink = true, $allowshortcodes = true) {
-		$result = '';
-		$content_full = get_post_field('post_content', $postid);
-		if ($allowshortcodes === false) {
-			$content_full = preg_replace('/\[.+\]/','', $content_full);
-		}
-		$content_full = apply_filters('the_content', $content_full);
-		$content_full = str_replace(']]>', ']]&gt;', $content_full);
-		$totalWordsAsArray = explode(' ', $content_full, $limit);
-		if (count($totalWordsAsArray)>=$limit) {
-			$imploded = implode(" ",$totalWordsAsArray);
-			$result .= strip_tags($imploded,'<br />');
-			$result .= '&hellip;';
-			if ($showmorelink) {$result .= ' <a class="more-link" href="'. get_permalink($postid) . '">Читать далее...</a>';}
-		} else {
-			$result .= $content_full;
-		}
-		return $result;
-	}
-
-	function content($limit, $postid, $showmorelink = true, $allowshortcodes = true) { //Normally, the second parameter provided is '$post->ID'
-			$content = explode(' ', get_post_field('post_content', $postid), $limit);
-			if (count($content)>=$limit) {
-				array_pop($content);
-				$content = implode(" ",$content);
-				if ($allowshortcodes === false) {
-				$content = preg_replace('/\[.+\]/','', $content);
-				}
-				$content = apply_filters('the_content', $content);
-					$content = str_replace(']]>', ']]&gt;', $content);
-				$content = strip_tags($content,'<br />');
-				$content .= '&hellip;';
-				if ($showmorelink) {$content .= ' <a class="more-link" href="'. get_permalink($postid) . '">Читать далее...</a>';}
-			} else {
-				$content = implode(" ",$content);
-				if ($allowshortcodes === false) {
-					$content = preg_replace('/\[.+\]/','', $content);
-				}
-				$content = apply_filters('the_content', $content);
-				$content = str_replace(']]>', ']]&gt;', $content);
-			}
-			return $content;
-		}
-
-
-	//Произвольная конвертация русских дат (например, в метах)
-	function dateToRussian($date) {
-		$month = array("january"=>"января", "february"=>"февраля", "march"=>"марта", "april"=>"апреля", "may"=>"мая", "june"=>"июня", "july"=>"июля", "august"=>"августа", "september"=>"сентября", "october"=>"октября", "november"=>"ноября", "december"=>"декабря");
-		$days = array("monday"=>"Понедельник", "tuesday"=>"Вторник", "wednesday"=>"Среда", "thursday"=>"Четверг", "friday"=>"Пятница", "saturday"=>"Суббота", "sunday"=>"Воскресенье");
-		return str_replace(array_merge(array_keys($month), array_keys($days)), array_merge($month, $days), strtolower($date));
-	}
-
-	//Add custom body classes to the chosen templates. 
-	//Usage: add_filter('body_class','add_bodyclass_customarchive'); in the template code
-	function add_bodyclass_newsarchive($classes = '') {
-		$classes[] = 'news';
-		return $classes;
-	}
-
-	//Get first paragraph
-	function get_first_paragraph(){
-		global $post;
-		$str = wpautop( get_the_content() );
-		$str = apply_filters('the_content', $str);
-		preg_match('#<p>(.*?)</p>#i', $str, $matches);
-		if (!empty($matches)) {
-			$str = $matches[0];
-			$str = strip_tags($str, '<a><strong><em>');
-			return '<p>' . $str . '</p>';
-		} else {
-			return false;
-		}
-	}
+  //Register widget areas
+  function widgets_init_now() {
+    register_sidebar( array(
+      'name' => 'Правая колонка'
+      ,'id' => 'aside-right'
+    ) );
+  }
+  add_action( 'widgets_init', 'widgets_init_now' );
 
 
 
-	/* ========================================================================================================================
-	
-	Register additional custom post types and taxonomies
-	
-	======================================================================================================================== */
+  /* ========================================================================================================================
+  
+  Actions and filters
+  
+  ======================================================================================================================== */
 
-	//
+  add_action( 'wp_enqueue_scripts', 'starkers_script_enqueuer' );
+
+
+  add_filter( 'body_class', array( 'Starkers_Utilities', 'add_slug_to_body_class' ) );
+
+
+  // Prepend to body classes prefix `p-`
+  add_filter( 'body_class', 'add_namespaced_body_classes' );
+  function add_namespaced_body_classes( $classes ) {
+    foreach ($classes as $k => $v) {
+      $classes[$k] = 'p-'.$v;
+    }
+    return $classes;
+  }
+
+
+  //makes all classes in custom menu dissappear, except noted
+  function css_attributes_filter($var) {
+    return is_array($var) ? array_intersect($var, array('current-menu-item','current_page_item','current-page-ancestor','current-menu-ancestor','current-menu-parent')) : '';
+  }
+  add_filter('nav_menu_css_class', 'css_attributes_filter', 100, 1); 
+  add_filter('nav_menu_item_id', 'css_attributes_filter', 100, 1);
 
 
 
-	/* ========================================================================================================================
-	
-	Customized functions
-	
-	======================================================================================================================== */
+  /* ========================================================================================================================
+  
+  Admin changes
+  
+  ======================================================================================================================== */
 
-	//
+  //Remove comments from admin
+  function remove_menus(){
+    remove_menu_page( 'edit-comments.php' );//Comments
+  }
+  add_action( 'admin_menu', 'remove_menus' );
+
+
+
+  /* ========================================================================================================================
+  
+  Reusable functions
+  
+  ======================================================================================================================== */
+
+  function custom_is_archive() {return (is_archive() || is_search());} // || is_page_template('template-custompage.php')
+
+
+  //Custom content function with words manual limit
+  function content($limit, $postid, $showmorelink = true, $allowshortcodes = true) {
+    $result = '';
+    $content_full = get_post_field('post_content', $postid);
+    if ($allowshortcodes === false) {
+      $content_full = preg_replace('/\[.+\]/','', $content_full);
+    }
+    $content_full = apply_filters('the_content', $content_full);
+    $content_full = str_replace(']]>', ']]&gt;', $content_full);
+    $totalWordsAsArray = explode(' ', $content_full, $limit);
+    if (count($totalWordsAsArray)>=$limit) {
+      $imploded = implode(" ",$totalWordsAsArray);
+      $result .= strip_tags($imploded,'<br />');
+      $result .= '&hellip;';
+      if ($showmorelink) {$result .= ' <a class="more-link" href="'. get_permalink($postid) . '">Читать далее...</a>';}
+    } else {
+      $result .= $content_full;
+    }
+    return $result;
+  }
+
+  function content($limit, $postid, $showmorelink = true, $allowshortcodes = true) { //Normally, the second parameter provided is '$post->ID'
+      $content = explode(' ', get_post_field('post_content', $postid), $limit);
+      if (count($content)>=$limit) {
+        array_pop($content);
+        $content = implode(" ",$content);
+        if ($allowshortcodes === false) {
+        $content = preg_replace('/\[.+\]/','', $content);
+        }
+        $content = apply_filters('the_content', $content);
+          $content = str_replace(']]>', ']]&gt;', $content);
+        $content = strip_tags($content,'<br />');
+        $content .= '&hellip;';
+        if ($showmorelink) {$content .= ' <a class="more-link" href="'. get_permalink($postid) . '">Читать далее...</a>';}
+      } else {
+        $content = implode(" ",$content);
+        if ($allowshortcodes === false) {
+          $content = preg_replace('/\[.+\]/','', $content);
+        }
+        $content = apply_filters('the_content', $content);
+        $content = str_replace(']]>', ']]&gt;', $content);
+      }
+      return $content;
+    }
+
+
+  //Произвольная конвертация русских дат (например, в метах)
+  function dateToRussian($date) {
+    $month = array("january"=>"января", "february"=>"февраля", "march"=>"марта", "april"=>"апреля", "may"=>"мая", "june"=>"июня", "july"=>"июля", "august"=>"августа", "september"=>"сентября", "october"=>"октября", "november"=>"ноября", "december"=>"декабря");
+    $days = array("monday"=>"Понедельник", "tuesday"=>"Вторник", "wednesday"=>"Среда", "thursday"=>"Четверг", "friday"=>"Пятница", "saturday"=>"Суббота", "sunday"=>"Воскресенье");
+    return str_replace(array_merge(array_keys($month), array_keys($days)), array_merge($month, $days), strtolower($date));
+  }
+
+  //Add custom body classes to the chosen templates. 
+  //Usage: add_filter('body_class','add_bodyclass_customarchive'); in the template code
+  function add_bodyclass_newsarchive($classes = '') {
+    $classes[] = 'news';
+    return $classes;
+  }
+
+  //Get first paragraph
+  function get_first_paragraph(){
+    global $post;
+    $str = wpautop( get_the_content() );
+    $str = apply_filters('the_content', $str);
+    preg_match('#<p>(.*?)</p>#i', $str, $matches);
+    if (!empty($matches)) {
+      $str = $matches[0];
+      $str = strip_tags($str, '<a><strong><em>');
+      return '<p>' . $str . '</p>';
+    } else {
+      return false;
+    }
+  }
+
+
+
+  /* ========================================================================================================================
+  
+  Register additional custom post types and taxonomies
+  
+  ======================================================================================================================== */
+
+  //
+
+
+
+  /* ========================================================================================================================
+  
+  Customized functions (walkers etc.)
+  
+  ======================================================================================================================== */
+
+  //
 
 
--- parts/footer.php
+++ parts/footer.php
@@ -1,5 +1,5 @@
 	</div> <!-- /content-area -->
 </div> <!-- /main -->
-<footer class="footer">
+<footer class="c-footer">
 	&copy; <?php echo date("Y"); ?> <?php bloginfo( 'name' ); ?>. All rights reserved.
 </footer>
